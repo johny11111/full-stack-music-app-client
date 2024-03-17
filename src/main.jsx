59357d3e255a5816aa8b5efb6b7a760224c13ef8
johny11111 <125285264+johny11111@users.noreply.github.com>
2024-03-17 04:51:19 +0200

    main-jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter as Router } from "react-router-dom"
-import { StateProvider } from './context/StateProvider'
-import { initialState } from './context/initialState'
-import  reducer  from './context/reducer'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import { BrowserRouter as Router } from "react-router-dom";
+import { StateProvider } from './context/StateProvider';
+import { initialState } from './context/initialState';
+import reducer from './context/reducer';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
-    <Router >
-      <StateProvider initialState={initialState} reducer={reducer}>
-        <App />
-      </StateProvider>
-    </Router>
-
-  </>
-)
+ReactDOM.render(
+  <Router>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
+  </Router>,
+  document.getElementById('root')
+);
