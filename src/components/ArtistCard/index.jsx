2368b46e91b83ef2403c47371e78c7b6fcdb4daf
johny11111 <125285264+johny11111@users.noreply.github.com>
2024-03-25 17:51:28 +0200

    create artist page & artist card add to home page & style

--- src/components/ArtistCard/index.jsx
+++ src/components/ArtistCard/index.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import styles from './style.module.css'
+import { Link } from 'react-router-dom'
+import { useStateValue } from '../../context/StateProvider'
+import { reducerCases } from '../../context/constants'
 
-export default function ArtistCard({artist}) {
+export default function ArtistCard({ artist }) {
+  const[{}, dispatch] = useStateValue()
+
+  const handleArtistClick = async () => {
+    return dispatch({type: reducerCases.SET_ARTISTS_SELECTED , artistSelected : artist  })
+  }
+
+
   return (
-    <div className={styles.containerArtistCard}>
+    <Link onClick={handleArtistClick} to={"artist"} className={styles.containerArtistCard}>
       <img src={artist?.image} alt="artistImg" />
       <p>{artist?.name}</p>
-    </div>
+    </Link>
   )
 }
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from 'react';
 import styles from "./style.module.css";
 import NavSide from '../NavSide';
 import { useStateValue } from '../../context/StateProvider';
-import { getAllAlbums } from '../../api';
+import { getAllAlbums, getAllArtists } from '../../api';
 import { reducerCases } from '../../context/constants';
+import ArtistCard from "../ArtistCard"
 import AlbumPage from '../AlbumPage';
 import { motion } from 'framer-motion'
 
@@ -11,27 +12,32 @@ import AlbumCard from '../AlbumCard';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Search from '../Search';
 import User from "../User"
+import ArtistPage from '../ArtistPage';
 export default function Home({ setScreenTime, screenTime, currentSongIndex, setCurrentSongIndex, setCurrentTime,
     currentTime }) {
 
 
 
-    const [{ user, albums, audioRef }, dispatch] = useStateValue();
+    const [{ user, albums, audioRef, artists, artistSelected }, dispatch] = useStateValue();
     const [albumsInSpanish, setAlbumsInSpanish] = useState(null)
     const [albumsInHebrew, setAlbumsInHebrew] = useState(null)
 
 
     useEffect(() => {
-        if (user === null) {
-            <div style={{ "width": "100vw", "position": "absolute", "top": "0" , "height": "100vh" }}>hello world</div>
+        if (!artistSelected) {
+            async function fetchData() {
+                await getAllArtists().then((data) => {
+                    console.log(data);
+                    dispatch({ type: reducerCases.SET_ARTISTS, artists: data.artist });
+                });
+            }
+            fetchData()
         }
-    }, [user])
+    }, [])
+
 
     useEffect(() => {
         async function fetchData() {
-            if (user === null) {
-                return <div style={{ "width": "100vw", "position": "absolute" }}>hello world</div>
-            }
             await getAllAlbums().then((data) => {
 
                 dispatch({ type: reducerCases.SET_ALBUMS, albums: data.album });
@@ -68,16 +74,24 @@ export default function Home({ setScreenTime, screenTime, currentSongIndex, setC
                 </motion.div>
                 <div className={styles.bodyContent}>
                     <Routes>
+                        <Route path='/artist' element={<ArtistPage />} />
                         <Route path='/user' element={<User />} />
                         <Route path='/search' element={<Search audioRef={audioRef} setCurrentSongIndex={setCurrentSongIndex} currentSongIndex={currentSongIndex} />} />
                         <Route path='/' element={<div className={styles.containerSongsH}>
                             <div className={styles.containerHomeContent}>
-                                <div style={{ "height": "78lvh", "overflowY": "scroll", "direction": "rtl", "padding": "0.5rem" }} >
+                                <div className={styles.homeBodyContent} >
+
                                     <h3>אלבומים בספרדית</h3>
                                     <div className={styles.containerTitle}>
                                         {albumsInSpanish && albumsInSpanish.map((album, i) => (
                                             <AlbumCard key={album._id} album={album} i={i} />
                                         ))}
+                                    </div>
+
+                                    <h3>אמנים</h3>
+                                    <div className={styles.containerArtist}>
+                                        {artists && artists.map((artist, i) => <ArtistCard key={artist._id} artist={artist} i={i} />
+                                        )}
                                     </div>
 
                                     <h3>אלבומים בעברית</h3>
